4231e8c75160d0e0b495add641d076301df22d4b
Jeff Huang <jeff@dinngo.co>
2023-08-03 15:33:08 +0800

    fix: change bps to 100 for collateral swap e2e test

--- test/transactions/collateral-swap.ts
+++ test/transactions/collateral-swap.ts
@@ -81,7 +81,7 @@ describe('Transaction: Collateral Swap', function () {
     expect(targetBalance.gte(minTargetAmount)).to.be.true;
   });
 
-  function bpsBound(amount: string, bps = 2, bpsBase = 10000): [string, string] {
+  function bpsBound(amount: string, bps = 100, bpsBase = 10000): [string, string] {
     const amountBigNum = BigNumberJS(amount);
     const offset = amountBigNum.times(bps).div(bpsBase);
     const max = amountBigNum.plus(offset);
