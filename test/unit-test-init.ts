1c5fa3fa3d6da87536137d60b05010836f128a82
Jeff Huang <jeff@dinngo.co>
2023-10-26 17:34:00 +0800

    fix: add mainnet rpc url in unit-test-init.ts

--- test/unit-test-init.ts
+++ test/unit-test-init.ts
@@ -1,4 +1,5 @@
 import * as common from '@protocolink/common';
 
+common.setNetwork(common.ChainId.mainnet, { rpcUrl: 'https://eth.llamarpc.com' });
 common.setNetwork(common.ChainId.polygon, { rpcUrl: 'https://rpc.ankr.com/polygon' });
 common.setNetwork(common.ChainId.arbitrum, { rpcUrl: 'https://arbitrum.llamarpc.com' });
